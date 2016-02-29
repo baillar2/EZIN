69a6fcaeca11712a4c12ac89db23d3497c73de53
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-02-29 16:20:17 -0700

    file picker works

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ var app = express()
 
 //APP CONFIG\\
 app.use(logger('dev'))
-app.use(bodyParser.JSON())	
+app.use(bodyParser.json())	
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname +'/public'))
 
 //ROUTES\\
 
 app.get('/', function(req, res){
-	res.sendFile('/index.html', {roor: './public'})
+	res.sendFile('/index.html', {root: './public'})
+})
 
-	//ACTIVATES SERVER\\
-	var port = 300
-	app.listen(port, function(){
-		console.log('Server is running on port ' + port)
-	})
+//ACTIVATES SERVER\\
+var port = 3000
+app.listen(port, function(){
+	console.log('Server is running on port ' + port)
 })
